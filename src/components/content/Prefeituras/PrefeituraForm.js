bc4fbd363ab98fa8b255e2e9e5f29443d021c456
wendellunny <wendelferreiraportugal@gmail.com>
2021-12-03 13:39:35 -0300

    Utilizando o react select

--- src/components/content/Prefeituras/PrefeituraForm.js
+++ src/components/content/Prefeituras/PrefeituraForm.js
@@ -4,10 +4,16 @@ import Select from 'react-select'
 
 export function PrefeituraForm(){
     const [estados,setEstados] = useState([]);
+    const [inputEstados,setInputEstados] = useState('');
+    const [cidades,setCidades] = useState([]);
 
     useEffect(()  => {
         getAllUF();
     },[]);
+
+    useEffect(()=>{
+        getUfCities(inputEstados);
+    },[inputEstados])
  
     return (
         
@@ -20,12 +26,11 @@ export function PrefeituraForm(){
 
                 <div className="form-group">
                     <label>Estado</label>
-                    <Select options={estados} />
+                    <Select options={estados} onChange={(value)=>setInputEstados(value.value)}/>
                 </div>
-
                 <div className="form-group">
-                    <labe>Razão Social</labe>
-                    <input type="text"/>
+                    <label>Cidade</label>
+                    <Select options={cidades} />
                 </div>
 
             </form>
@@ -47,6 +52,18 @@ export function PrefeituraForm(){
         setEstados(options);
     }
     
+    async function getUfCities(uf){
+        const options = [];
+        const response = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`);
+        const data = await response.json();
+
+        data.forEach(city => {
+            options.push({value:city.id, label: city.nome})    
+        });
+
+        setCidades(options);
+    
+    }
 }
 
 
